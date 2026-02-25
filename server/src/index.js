009d7b0a9cfa98986d3734c089790bc7319f072d
MD. KHARUL ISLAM SHAGOR <sr01885013972@gmail.com>
2026-02-25 15:40:08 +0600

    second commit

--- server/src/index.js
+++ server/src/index.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: [process.env.CLIENT_URL || 'http://localhost:5173', process.env.ADMIN_URL || 'http://localhost:5174'],
+  origin: [process.env.CLIENT_URL || 'https://shagor-port-folio.vercel.app', process.env.ADMIN_URL || 'https://portfolio-admin-orpin.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
